ff27882af97d175958442318583a5fb83d1f7ef5
EsaAdeliaN <esadelvi354@gmail.com>
2023-04-03 12:09:28 +0700

    first

---
+++ komentar.js
@@ -0,0 +1,6 @@
+// ini merupakan komentar satu baris, kode dibawah ini tidak akan dijalankan
+// console.log("Halo!");
+/* ini merupakan komentar dengan lebih dari satu baris 
+Teks apapun yang berada disini akan dijadikan komentar.
+Ketika menggunakan ini, jangan lupa untuk menutupnya.
+*/
---
+++ variabel.js
@@ -0,0 +1,9 @@
+var minuman;
+minuman = "es jeruk";
+console.log(minuman);
+
+let firstname ="esa";
+console.log(firstname);
+
+const a = "20";
+console.log(a);
